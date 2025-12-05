1c2618317c0212d0646268e1024a428f835a6a97
Nishant Gajbhiye <nishantgajbhiye71@gmail.com>
2025-12-05 12:54:48 +0530

    Learn about Sub arrays

---
+++ src/SubArray.java
@@ -0,0 +1,22 @@
+public class SubArray {
+    public static void Subarrays(int numbers[]) {
+        int tp = 0;
+        for (int i=0; i<numbers.length; i++){
+            int start = i;
+            for (int j=1; j<numbers.length; j++){
+                int end = j;
+                for (int k=start; k<=end; k++){
+                    System.out.print(numbers[k]+" ");
+                }
+                tp++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("Total subarray = " + tp);
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,8,10};
+        Subarrays(numbers);
+    }
+}
